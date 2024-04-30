ab9d0dbeec3b9064358781d3017f2bdc93716207
Anant basu <anant@paymonk.com>
2024-04-30 14:21:50 +0530

    simple changes

--- recharge/src/main/java/com/example/recharge/SimpleToast.kt
+++ recharge/src/main/java/com/example/recharge/SimpleToast.kt
@@ -3,7 +3,7 @@ package com.example.recharge
 import android.content.Context
 import android.widget.Toast
 
-open class SimpleToast {
+public class SimpleToast {
     companion object{
         fun showToast(msg:String?,context: Context?){
             Toast.makeText(context,msg,Toast.LENGTH_SHORT).show()
